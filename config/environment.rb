dc60be3a94c54a6c72b0afdb65180d2e38513d97
pwjablonski <pwjablonski@gmail.com>
2015-11-03 16:53:29 -0500

    mailer almost working

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,15 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+#mailer setup
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+    :tls => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => :login,
+    :user_name => "pwjablonski",
+    :password => "Baidu1991"
+}
