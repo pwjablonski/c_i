7afc9c211115732a16852c4cb709e2dfb5cba284
pwjablonski <pwjablonski@gmail.com>
2015-12-11 03:24:36 +0000

    working enrollment

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -3,9 +3,10 @@ class EnrollmentsController < ApplicationController
 
     def create
         
+        @classroom = Classroom.find(params[:classroom_id])
+        if User.find_by(email: params[:student_email]).try(:student?)
             @student = User.find_by(email: params[:student_email]).student
 #           @student = User.find_by(email: params[:enrollment][:student_id]).student
-            @classroom = Classroom.find(params[:classroom_id])
             @enrollment = nil
             if @student != nil
              @enrollment = @classroom.add_student(@student)
@@ -19,6 +20,13 @@ class EnrollmentsController < ApplicationController
                 format.html { redirect_to classroom_path(@classroom),
                     notice: 'Student Was Not Enrolled!' }
 
+            end
+        end
+            
+        else
+        respond_to do |format|
+                format.html { redirect_to classroom_path(@classroom),
+                    notice: 'Student was not found!' }
             end
         end
     end
