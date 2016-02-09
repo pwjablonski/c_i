561fefaef31267c48c66def80edf61e9067165fb
pwjablonski <pwjablonski@gmail.com>
2016-02-09 16:31:44 -0500

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   
   config.action_mailer.perform_deliveries = true
   
-  config.action_mailer.default_url_options = { host: 'localhost:3000'}
+  config.action_mailer.default_url_options = { host: 'https://weareci.herokuapp.com/'}
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
