9a9b8549b49488d371f3938bf1783f390474f2db
pwjablonski <pwjablonski@gmail.com>
2016-02-09 19:32:52 -0500

    bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,11 +9,11 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require ace-rails-ap
+//= require bootstrap
 //= require_tree .
 
 $(function() {
