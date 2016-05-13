152c4e13b57800b776b1d27a0fbc6fccb7a0954d
pwjablonski <pwjablonski@gmail.com>
2016-05-13 13:55:34 -0400

    precomplie

--- Gemfile
+++ Gemfile
@@ -109,3 +109,4 @@ gem 'select2-rails', '~> 4.0', '>= 4.0.1'
 gem 'momentjs-rails', '>= 2.9.0'
 gem 'bootstrap3-datetimepicker-rails', '~> 4.17.37'
 
+gem 'sprockets_better_errors'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   config.action_view.raise_on_missing_translations = true
   
+  config.assets.raise_production_errors = true
   
   config.action_mailer.perform_deliveries = true
   
